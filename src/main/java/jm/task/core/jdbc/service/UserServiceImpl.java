ec7edb3bbc07a440eb67694047638d753d25aa23
alekseibakhov <78568380+alekseibakhov@users.noreply.github.com>
2021-05-23 00:02:33 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,181 +1,39 @@
 package jm.task.core.jdbc.service;
-
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
-public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
-    Connection connection = new Util().getConnection();
+
+public class UserServiceImpl extends UserDaoHibernateImpl implements UserService {
+    UserDao ud = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS UsersTable\n" +
-                "( id int PRIMARY KEY AUTO_INCREMENT,\n" +
-                "  name char(50) NOT NULL,\n" +
-                "  lastName char(50),\n" +
-                "  age int\n" +
-                ") ;";
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            statement.execute(sql);
-            System.out.println("Таблица UsersTable создана");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
-
+        ud.createUsersTable();
     }
 
     public void dropUsersTable() {
-        Statement statement = null;
-        String sqlDel = "DROP TABLE IF EXISTS UsersTable";
-
-        try {
-            statement = connection.createStatement();
-            statement.execute(sqlDel);
-            System.out.println("Удалена таблица UsersTable");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
-
+        ud.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        createUsersTable();
-
-        PreparedStatement preparedStatement = null;
-
-        String sqlSave = "INSERT INTO UsersTable (name, lastName, age) VALUES (?, ?, ?)";
-
-        try {
-
-            preparedStatement = connection.prepareStatement(sqlSave);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-
-            preparedStatement.executeUpdate();
-            System.out.println("Пользователь " + name + " добавлен");
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
+        ud.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        PreparedStatement preparedStatement = null;
-        String sqlRem = "DELETE FROM UsersTable WHERE id = ?";
-
-        try {
-            preparedStatement = connection.prepareStatement(sqlRem);
-
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-
-            System.out.println("Удален пользователь с ID = " + id);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
+        ud.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        createUsersTable();
-        List<User> list = new ArrayList();
-        String sqrGet = "SELECT id, name, lastName, age FROM UsersTable";
-
-        Statement statement = null;
-
-        try {
-            statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sqrGet);
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-
-                list.add(user);
-            }
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
-        return list;
+        return ud.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        createUsersTable();
-        String sqlClean = "TRUNCATE TABLE userstable";
-        Statement statement = null;
-
-        try {
-            statement = connection.createStatement();
-            statement.execute(sqlClean);
-            System.out.println("Таблица очищена");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } finally {
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        }
-
+        ud.cleanUsersTable();
     }
 }
+
